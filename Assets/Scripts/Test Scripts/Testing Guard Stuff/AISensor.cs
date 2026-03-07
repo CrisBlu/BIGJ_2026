36d7f24eb2bd03e1f1d5a67e1b5badf11bd4af73
Felix Jarquin <fjarq005@fiu.edu>
2026-03-07 18:55:14 -0500

    Sensor works for gizmos

--- Assets/Scripts/Test Scripts/Testing Guard Stuff/AISensor.cs
+++ Assets/Scripts/Test Scripts/Testing Guard Stuff/AISensor.cs
@@ -8,13 +8,77 @@ public class AISensor : MonoBehaviour
     public float angle = 30;
     public float height = 1.0f;
     public Color meshColor = Color.red;
+    public int scanFrequency = 30;
+    public LayerMask layers;
+    public List<GameObject> Objects = new List<GameObject>();
+
+    Collider[] colliders = new Collider[50];
     Mesh mesh;
-    Mesh CreateWedgeMesh()
+    Mesh gameMesh;
+    int count;
+    float scanInterval;
+    float scanTimer;
+
+    [Header("Mesh In Game")] public bool showInGame = true;
+    [Range(0f, 5f)] public float gameHeight = 0.05f;
+
+    void Start()
+    {
+        scanInterval = 1.0f / scanFrequency;
+    }
+
+    void Update()
+    {
+        scanTimer -= Time.deltaTime;
+        if (scanTimer < 0)
+        {
+            scanTimer += scanInterval;
+            Scan();
+        }
+    }
+
+    private void Scan()
+    {
+        count = Physics.OverlapSphereNonAlloc(transform.position, distance, colliders, layers,
+            QueryTriggerInteraction.Collide);
+
+        Objects.Clear();
+        for (int i = 0; i < count; i++)
+        {
+            GameObject obj = colliders[i].gameObject;
+            if (IsInSight(obj))
+            {
+                Objects.Add(obj);
+            }
+        }
+    }
+
+    public bool IsInSight(GameObject obj)
+    {
+        Vector3 origin = transform.position;
+        Vector3 dest = obj.transform.position;
+        Vector3 dir = dest - origin;
+        if (dir.y < 0 || dir.y > height)
+        {
+            return false;
+        }
+
+        dir.y = 0;
+        float deltaAngle = Vector3.Angle(dir, transform.forward);
+        if (deltaAngle > angle)
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+
+Mesh CreateWedgeMesh(float h)
     {
         Mesh mesh = new Mesh();
 
         int segments = 10;
-        
         int numTriangles = (segments * 4) + 2 + 2;
         int numVertices = numTriangles * 3;
         
@@ -22,69 +86,59 @@ public class AISensor : MonoBehaviour
         int[] triangles = new int[numVertices];
         
         Vector3 bottomCenter = Vector3.zero;
-        Vector3 bottomLeft = Quaternion.Euler(0,-angle, 0) * Vector3.forward * distance;
-        Vector3 bottomRight = Quaternion.Euler(0,angle, 0) * Vector3.forward * distance;
+        Vector3 bottomLeft = Quaternion.Euler(0, -angle, 0) * Vector3.forward * distance;
+        Vector3 bottomRight = Quaternion.Euler(0, angle, 0) * Vector3.forward * distance;
         
-        Vector3 topCenter = bottomCenter + Vector3.up * height;
-        Vector3 topRight = bottomRight + Vector3.up * height;
-        Vector3 topLeft = bottomLeft + Vector3.up * height;
+        Vector3 topCenter = bottomCenter + Vector3.up * h;
+        Vector3 topRight = bottomRight + Vector3.up * h;
+        Vector3 topLeft = bottomLeft + Vector3.up * h;
 
         int vert = 0;
         
-        //left side
         vertices[vert++] = bottomCenter;
         vertices[vert++] = bottomLeft;
         vertices[vert++] = topLeft;
-        
         vertices[vert++] = topLeft;
         vertices[vert++] = topCenter;
-        vertices[vert++] = bottomCenter;        
-        //right side
+        vertices[vert++] = bottomCenter;
+
         vertices[vert++] = bottomCenter;
         vertices[vert++] = topCenter;
         vertices[vert++] = topRight;
-        
         vertices[vert++] = topRight;
         vertices[vert++] = bottomRight;
         vertices[vert++] = bottomCenter;
         
         float currentAngle = -angle;
-        float deltaAngle = (angle *2) / segments;
+        float deltaAngle = (angle * 2) / segments;
         for (int i = 0; i < segments; ++i)
         {
-            bottomLeft = Quaternion.Euler(0,currentAngle, 0) * Vector3.forward * distance;
-            bottomRight = Quaternion.Euler(0,currentAngle + deltaAngle, 0) * Vector3.forward * distance;
+            bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward * distance;
+            bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward * distance;
             
-            topRight = bottomRight + Vector3.up * height;
-            topLeft = bottomLeft + Vector3.up * height;
+            topRight = bottomRight + Vector3.up * h;
+            topLeft = bottomLeft + Vector3.up * h;
             
             currentAngle += deltaAngle;
             
-            //far side
             vertices[vert++] = bottomLeft;
             vertices[vert++] = bottomRight;
             vertices[vert++] = topRight;
-        
             vertices[vert++] = topRight;
             vertices[vert++] = topLeft;
             vertices[vert++] = bottomLeft;
         
-            //top
             vertices[vert++] = topCenter;
             vertices[vert++] = topLeft;
             vertices[vert++] = topRight;
         
-            //bottom
             vertices[vert++] = bottomCenter;
             vertices[vert++] = bottomRight;
             vertices[vert++] = bottomLeft;
-
         }
 
         for (int i = 0; i < numVertices; ++i)
-        {
             triangles[i] = i;
-        }
         
         mesh.vertices = vertices;
         mesh.triangles = triangles;
@@ -93,11 +147,22 @@ public class AISensor : MonoBehaviour
         return mesh;
     }
 
-
     private void OnValidate()
     {
-        mesh = CreateWedgeMesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        scanInterval = 1.0f/scanFrequency;
+        mesh = CreateWedgeMesh(height);
+        gameMesh = CreateWedgeMesh(gameHeight);
+
+        var mf = GetComponent<MeshFilter>();
+        if (mf != null) mf.mesh = gameMesh;
+
+        var mr = GetComponent<MeshRenderer>();
+        if (mr != null)
+        {
+            mr.enabled = showInGame;
+            if (mr.sharedMaterial != null)
+                mr.sharedMaterial.color = meshColor;
+        }
     }
 
     private void OnDrawGizmos()
@@ -107,7 +172,17 @@ public class AISensor : MonoBehaviour
             Gizmos.color = meshColor;
             Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
         }
+        Gizmos.DrawWireSphere(transform.position, distance);
+        for (int i = 0; i < count; ++i)
+        {
+            Gizmos.DrawSphere(colliders[i].transform.position, 0.5f);
+        }
+        
+        Gizmos.color = Color.green;
+        foreach (var obj in Objects)
+        {
+            Gizmos.DrawSphere(obj.transform.position, 0.5f);
+        }
+        
     }
-    
-    
 }
